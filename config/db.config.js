ab8afb28622ec917495f2d105d0244d386cc21a5
CarlosFastLab <fastcaf@hotmail.com>
2021-06-14 20:22:10 -0300

    db.config updated for heroku db

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "postgres",
-    DB: "testdb",
+    HOST: "ec2-54-197-100-79.compute-1.amazonaws.com",
+    USER: "hbeifwwsixezml",
+    PASSWORD: "6a907a8304c00b34128b75c74b2cf83beb820a1a944629e14d887c932598d647",
+    DB: "dd17odemp2gvne",
     dialect: "postgres",
     pool: {
         max: 5,

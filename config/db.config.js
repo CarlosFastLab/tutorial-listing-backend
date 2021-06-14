78b7a7f096d94bd3c07d2c112ff73cfb058134d8
CarlosFastLab <fastcaf@hotmail.com>
2021-06-14 20:32:55 -0300

    adding ssl to sequelize

--- config/db.config.js
+++ config/db.config.js
@@ -4,6 +4,10 @@ module.exports = {
     PASSWORD: "6a907a8304c00b34128b75c74b2cf83beb820a1a944629e14d887c932598d647",
     DB: "dd17odemp2gvne",
     dialect: "postgres",
+    ssl: true,
+    dialectOptions: {
+        ssl: true
+    },
     pool: {
         max: 5,
         min: 0,
--- models/index.js
+++ models/index.js
@@ -5,12 +5,14 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
     operatorsAliases: false,
+    ssl: dbConfig.ssl,
+    dialectOptions: dbConfig.dialectOptions,
 
     pool: {
         max: dbConfig.pool.max,
         min: dbConfig.pool.min,
         acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.idle
+        idle: dbConfig.pool.idle,
     }
 });
 
